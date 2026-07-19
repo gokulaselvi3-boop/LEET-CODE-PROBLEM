64cabec77c80d4f206d5778745b64b5b977485bc
gokulaselvi3-boop <gokulaselvi3@gmail.com>
2026-07-19 23:35:20 +0530

    Create DAY2 225 Implement Stack using Queues.java

---
+++ DAY2 225 Implement Stack using Queues.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class MyStack {
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.offer(x);
+
+        // Move previous elements behind the new element
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.offer(q.poll());
+        }
+    }
+
+    public int pop() {
+        return q.poll();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
