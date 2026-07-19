c0f83fc518ac82533f9847773e67b9cd21f6409a
gokulaselvi3-boop <gokulaselvi3@gmail.com>
2026-07-20 00:19:57 +0530

    Create DAY6 65 Valid Number.java

---
+++ DAY6 65 Valid Number.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isNumber(String s) {
+        s = s.trim();
+
+        boolean seenDigit = false;
+        boolean seenDot = false;
+        boolean seenExp = false;
+        boolean seenDigitAfterExp = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                seenDigit = true;
+                if (seenExp) {
+                    seenDigitAfterExp = true;
+                }
+            } else if (c == '.') {
+                if (seenDot || seenExp) {
+                    return false;
+                }
+                seenDot = true;
+            } else if (c == 'e' || c == 'E') {
+                if (seenExp || !seenDigit) {
+                    return false;
+                }
+                seenExp = true;
+                seenDigitAfterExp = false;
+            } else if (c == '+' || c == '-') {
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return seenDigit && seenDigitAfterExp;
+    }
+}
